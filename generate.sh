96696cd9c83af958a3c8d83d389e1134f302986e
Vitor George <vitor.george@gmail.com>
2013-12-16 20:09:29 -0200

    updated scripts

--- generate.sh
+++ generate.sh
@@ -6,9 +6,9 @@ ogr2ogr data/prodes.shp data/prodes-skeleton.shp -t_srs EPSG:4326
 for f in data/expanded/*.shp
 do
   echo "Processing $f file.."; 
-  ogr2ogr -update -append data/prodes.shp -nln prodes -select ano,view_date,area,areameters,class_name -t_srs EPSG:4326 $f  
+  ogr2ogr -update -append data/prodes.shp -nln prodes -select ano,view_date,julday,area,areameters,class_name -t_srs EPSG:4326 $f  
 done
 
 # start a new db
 rm data/prodes.sqlite
-spatialite data/prodes.sqlite < lib/sql/import.sql
+spatialite data/prodes.sqlite < import.sql
--- import.sql
+++ import.sql
@@ -5,16 +5,35 @@
 SELECT CreateSpatialIndex('prodes', 'geometry');
 CREATE INDEX area_prodes_index ON prodes (area);
 CREATE INDEX ano_prodes_index ON prodes (ano);
+CREATE INDEX areameters_prodes_index ON prodes (areameters);
+CREATE INDEX class_name_prodes_index ON prodes (class_name);
+CREATE INDEX view_date_prodes_index ON prodes (view_date);
+CREATE INDEX julday_name_prodes_index ON prodes (julday);
 
--- remove residue records
+-- remove residues records
 delete from prodes where class_name like 'res%';
 delete from prodes where class_name like 'RES%';
 
 -- update ano field
 update prodes set ano = cast(substr(view_date,1,4) as integer) where ano is 0;
 
--- round area
-update prodes set area = round(area) where area > 0;
+-- update area from areameters field
+update prodes set area = cast(areameters as real) where area = 0;
 
--- update area field
-update prodes set area = round(cast(areameters as real)) where area = 0;
+-- round area to hectares
+update prodes set area = round(area / 10000, 2) where area > 0;
+
+-- purge very small areas
+delete from prodes where area = 0;
+
+-- where missing view_date, calculate it from julday
+update prodes set view_date = strftime('%Y-%m-%d', julianday(ano || '-01-01') + julday - 1) where length(view_date)<=2 and julday>0;
+update prodes set view_date = ano || '-01-01' where length(view_date)<=2 and julday=0;
+	
+-- update year, month and day fields
+update prodes set year = strftime('%Y', view_date), month = strftime('%m', view_date), day = strftime('%d', view_date);
+
+-- update area_pt field using commas as decimal separator
+update prodes set area_pt = replace(area,'.',',');
+
+vacuum;
