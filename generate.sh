490d70ca05870e491349cc6ab1244c3315b72843
Vitor George <vitor.george@gmail.com>
2013-12-19 14:33:27 -0200

    generate scripts merged

--- generate.sh
+++ generate.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+## Download sources
+wget -r -nd -A "*AMZ_shp.zip" -c -P tmp/sources/zips http://www.dpi.inpe.br/prodesdigital/dadosn/mosaicos/
+
+# Expand them
+unzip ./tmp/sources/zips/\*.zip -d ./tmp/sources/shapefiles
+
+# Remove unnecessary
+rm tmp/sources/shapefiles/*Nuvem*
+rm tmp/sources/shapefiles/*NUVEM*
+rm tmp/sources/shapefiles/*Residuo*
+
 # Merge shapefiles
-rm data/prodes.*
-ogr2ogr data/prodes.shp data/prodes-skeleton.shp -t_srs EPSG:4326
-for f in data/expanded/*.shp
+rm tmp/sources/prodes.*
+ogr2ogr tmp/sources/prodes.shp templates/prodes-skeleton.shp -t_srs EPSG:4326
+for f in tmp/sources/shapefiles/*.shp
 do
   echo "Processing $f file.."; 
-  ogr2ogr -update -append data/prodes.shp -nln prodes -select ano,view_date,julday,area,areameters,class_name -t_srs EPSG:4326 $f  
+  ogr2ogr -update -append tmp/sources/prodes.shp -nln prodes -select ano,view_date,julday,area,areameters,class_name -t_srs EPSG:4326 $f  
 done
 
 # start a new db
 rm data/prodes.sqlite
-spatialite data/prodes.sqlite < import.sql
+spatialite data/prodes.sqlite < scripts/import.sql
